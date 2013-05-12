016809c946573168a89e421f86ed328f671e3233
Ronny Bruus <rnnbrs@gmail.com>
2013-05-12 22:09:49 +0200

    Showing user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
+  resources :users
+
   root to: 'static_pages#home'
-  
-  get "users/new"
   
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'

4533c050498eb2c0cc96147d92e43412a7559a68
Ronny Bruus <rnnbrs@gmail.com>
2013-05-19 22:37:56 +0200

    User profile page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,7 +3,15 @@ require 'spec_helper'
 describe "User pages" do
   subject{ page }
   
-  describe "Signup page" do
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+    
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+  
+  describe "signup page" do
     before{ visit signup_path }
     
     it{ should have_selector('h1', text: 'Sign up') }

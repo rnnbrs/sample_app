6b0afad5146dea7b64d38a25d826657b8d155d39
Ronny Bruus <rnnbrs@gmail.com>
2013-05-20 00:16:15 +0200

    User signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,9 +12,50 @@ describe "User pages" do
   end
   
   describe "signup page" do
-    before{ visit signup_path }
+    before { visit signup_path }
     
-    it{ should have_selector('h1', text: 'Sign up') }
-    it{ should have_selector('title', text: full_title('Sign up')) }
+    it { should have_selector('h1', text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+  end
+  
+  describe "signup" do
+    let(:submit) { "Create my account" }
+    before { visit signup_path }
+    
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+      
+      describe "after submission" do
+        before { click_button submit }
+        
+        it { should have_selector('title', text: "Sign up")}
+        it { should have_content('error') }
+      end
+    end
+    
+    describe "with valid information" do
+      let(:user_email) { "user@example.com" }
+      
+      before do
+        fill_in "Name", with: "Example user"
+        fill_in "Email", with: user_email
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+      end
+      
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+      
+      describe "after saving the user" do
+        let(:user) { User.find_by_email(user_email) }
+        before { click_button submit }
+        
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
   end
 end

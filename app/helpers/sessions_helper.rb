a5039746d0cba413b851721fd86fb01f46a36e2d
Ronny Bruus <rnnbrs@gmail.com>
2013-05-20 23:45:03 +0200

    Finish signin, signout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,4 +15,10 @@ module SessionsHelper
   def signed_in?
     !(current_user.nil? || current_user.remember_token.nil?)
   end
+  
+  
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
+  end
 end

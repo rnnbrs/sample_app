8e6bdf8485cc9efcb0cae6dd84bc6211e04e88b7
Ronny Bruus <rnnbrs@gmail.com>
2013-05-23 00:07:18 +0200

    Users view, admin property, tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,8 +23,19 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
   
   it { should be_valid }
+  it { should_not be_admin }
+  
+  describe "when admin attribute is set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    
+    it { should be_admin }
+  end
   
   describe "when name is not present" do
     before { @user = User.new( name: " ", email: "email@example.com" ) }
